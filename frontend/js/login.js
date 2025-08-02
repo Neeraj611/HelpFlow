41f4bf7a685e3306cd440b29e26bcf1e1857fd49
Samarth Bhatt <samarthbhatt029@gmail.com>
2025-08-02 14:28:19 +0530

    Delete frontend directory

--- frontend/js/login.js
+++
@@ -1,9 +0,0 @@
-// login.js (on successful login)
-const user = {
-  name: "Samarth Bhatt",
-  email: "samarth@example.com",
-  role: "admin" // or "support" or "user"
-};
-
-localStorage.setItem("helpflowUser", JSON.stringify(user));
-window.location.href = "profile.html";
--- frontend/js/register.js
+++
@@ -1,9 +0,0 @@
-//register.js (on successful register)
-const user = {
-  name: "Samarth Bhatt",
-  email: "samarth@example.com",
-  role: "admin" // or "support" or "user"
-};
-
-localStorage.setItem("helpflowUser", JSON.stringify(user));
-window.location.href = "profile.html";
